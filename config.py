c026173069ddfe6f5d137adac941951811aa3ea5
AlexSteelHammer <Alextor3511@gmail.com>
2023-02-15 15:56:56 +0300

    Telegram Bot converter money.

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '6274812275:AAGofoeza9ql1zBLNUlHDAnhlyDIPxY1d1k'
+
+keys = {'доллар': 'USD',
+        'евро': 'EUR',
+        'рубль': 'RUB',
+        'фунт': 'GBP',
+        'юань': 'CNY',
+}
